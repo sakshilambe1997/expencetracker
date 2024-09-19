58501f68b5b6ed5440f3b14f4bea769e12ffb4a1
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-09-20 00:41:41 +0530

    media query added to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,20 +10,20 @@ function Footer() {
     <>
       <div className="footer-container">
         <div className="footer-menu-container">
-          <h3>Company</h3>
+          <h3 className="footer-main-heading">Company</h3>
           <p className="footer-text">About Us</p>
           <p className="footer-text">Our Services</p>
           <p className="footer-text">Privacy Policy</p>
         </div>
 
         <div className="footer-menu-container">
-          <h3>Get Help</h3>
+          <h3 className="footer-main-heading">Get Help</h3>
           <p className="footer-text">FAQ</p>
           <p className="footer-text">Services</p>
         </div>
 
         <div className="footer-menu-container">
-          <h3 className="contact-us-heading">Contact Us</h3>
+          <h3 className="footer-main-heading">Contact Us</h3>
           <img src={Facebook}  className="footer-contact-logo"/>
           <img src={Instagram} className="footer-contact-logo"/>
           <img src={Twitter} className="footer-contact-logo"/>
@@ -31,7 +31,7 @@ function Footer() {
         </div>
 
         <div className="footer-menu-container">
-          <h3>Address</h3>
+          <h3 className="footer-main-heading">Address</h3>
           <p className="footer-text"> Shivaji Nagar,main road Pune.</p>
         </div>
       </div>
